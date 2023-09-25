a2a9167196caf763f45eede657b17b758111b354
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2023-09-25 11:05:41 +0500

    added the soluion of Question 2

---
+++ 02/index.ts
@@ -0,0 +1,8 @@
+// Question 2:
+// Personal Message: Store a person’s name in a variable,
+// and print a message to that person. Your message should be simple, such as, “Hello Eric,
+// would you like to learn some Python today?”
+
+let userName = `Muneeb`;
+let message = `Hello, ${userName}, would you like to Learn Some TypeScript Today?`;
+console.log(message);
