9dc5f00dc4d373d133d733391d74e71963a26cc1
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2023-09-25 12:28:06 +0500

    Corrected the Solution According to Question

--- 04/index.ts
+++ 04/index.ts
@@ -1,8 +1,7 @@
 /**Question # 4 */
 // Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
 // Your output should look something like the following, including the quotation marks:
+//  Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 
-let quote = `Complain not to your fellow creatures against the Creator, and complain not to the Creator against hour fellow creatures. You will attain to peace.`;
-let author = `Wasif Ali Wasif R.A`;
-let message = `${author} once Said, "${quote}"`;
-console.log(message);
+console.log(`Wasif Ali Wasif R.A once said, "Complain not to your fellow creatures against the Creator, and complain not to
+ the Creator against hour fellow creatures. You will attain to peace."`);
