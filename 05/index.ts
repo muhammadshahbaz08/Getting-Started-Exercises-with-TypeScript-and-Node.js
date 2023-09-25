58dadef36d87d4c127e9227d35ccb5de7d1462a1
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2023-09-25 12:33:25 +0500

    Added the Solution for Question 5

---
+++ 05/index.ts
@@ -0,0 +1,7 @@
+/**Question # 5*/
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+//Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person = `Wasif Ali Wasif R.A`;
+let message = `${famous_person} once Said, "Complain not to your fellow creatures against the Creator,and complain not to the Creator against hour fellow creatures. You will attain to peace."`;
+console.log(message);
