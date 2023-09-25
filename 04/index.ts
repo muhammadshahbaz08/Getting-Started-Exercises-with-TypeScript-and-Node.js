9f33dc422a3951faf008e03a1c3a69793b5e1c08
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2023-09-25 11:44:06 +0500

    Added The Solution for Question 4

---
+++ 04/index.ts
@@ -0,0 +1,8 @@
+/**Question # 4 */
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// Your output should look something like the following, including the quotation marks:
+
+let quote = `Complain not to your fellow creatures against the Creator, and complain not to the Creator against hour fellow creatures. You will attain to peace.`;
+let author = `Wasif Ali Wasif R.A`;
+let message = `${author} once Said, "${quote}"`;
+console.log(message);
