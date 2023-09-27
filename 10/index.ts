0d03f7c20ab64d13039ddb69b5692bdf1e6b7bab
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2023-09-27 11:21:20 +0500

    Added the Solution for Question 12

---
+++ 10/index.ts
@@ -0,0 +1,12 @@
+/**Question 10 
+ Adding Comments: Choose two of the programs you’ve written, 
+ and add at least one comment to each. If you don’t have anything specific to
+ write because your programs are too simple at this point,just add your name and 
+ the current date at the top of each program file.Then write one sentence describing what the program does
+*/
+
+// Question 9
+// Name : Shahbaz
+// Date : 9-27-2023
+let favNumber = 90;
+console.log(`My Favorite Number is ${favNumber}`);
---
+++ 12/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Question 12
+ * Start with the array you used in Exercise 11, but instead of just printing
+ * each person’s name,  print a message to them. The text of each message should be the same,
+ * but each message should be personalized with the person’s name..
+ */
+
+let names: string[] = ["Ali", "Ahmad", "Khaleel"];
+for (let i = 0; i < names.length; i++) {
+  console.log(`Hello! ${names[i]}`);
+}
