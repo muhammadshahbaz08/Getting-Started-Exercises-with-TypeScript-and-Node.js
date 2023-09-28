d81545cb0d2e8d77ea90c607f80b16cb8ba05ae6
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2023-09-28 16:00:43 +0500

    Solution For Question 19 Submitted

---
+++ 19/index.ts
@@ -0,0 +1,7 @@
+/** Question 19
+ * Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+ * print a message indicating the number of people you are inviting to dinner.
+ */
+// we are using question 14 guests array to tell how many people are invited to dinner,
+const guest: string[] = ["John", "Tyson", "Alex", "Ben"];
+console.log(`${guest.length} Guests are Invited to the Dinner`);
