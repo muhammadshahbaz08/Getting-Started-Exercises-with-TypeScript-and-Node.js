f4edae5e2237d08c9117772536b3bf14a0d21490
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2023-09-30 08:06:59 +0500

    Added Solution For Question 21

---
+++ 21/index.ts
@@ -0,0 +1,9 @@
+/** Question 21
+ * They think of something you could store in a TypeScript Object.
+ * Write a program that creates Objects containing these items.
+ */
+
+const city = {
+  cities: ["Lahore", "Karachi", "Rawalpindi", "Peshawar", "Quetta"],
+};
+console.log(city.cities);
