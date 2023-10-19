9ef633f67e598f1b524182ef50eb9948371a1876
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2023-10-19 13:00:44 +0500

    Added Solution for Quesion No.41

---
+++ 41/index.ts
@@ -0,0 +1,21 @@
+/** Question 41
+ * Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of
+ * each magician in the array.
+ */
+
+const magician_Names: string[] = [
+  "John",
+  "Alex",
+  "Jordan",
+  "Tim Pane",
+  "Joe",
+  "Ben",
+];
+
+const show_magicians = () => {
+  magician_Names.forEach((mag_name) => {
+    console.log(`Magician ${mag_name}`);
+  });
+};
+
+show_magicians();
